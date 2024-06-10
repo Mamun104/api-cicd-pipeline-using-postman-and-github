1d7c63d141cd8be1845fbb52ddec425fd8e60851
Mamun104 <muhammadmamunurrashid340@gmail.com>
2024-06-10 15:43:35 +0600

    upload project

---
+++ report.js
@@ -0,0 +1,16 @@
+const newman = require('newman');
+
+newman.run({
+    collection: require('./collection/dmoney-users.postman_collection.json'), // pointing to local JSON file.
+    iterationCount: 1,
+    reporters: 'htmlextra',
+    reporter: {
+        htmlextra: {
+            export: './Reports/report.html', // If not specified, the file will be written to `newman/` in the current working directory.
+        }
+    }
+    
+}, function (err) {
+    if (err) { throw err; }
+    console.log('collection run complete!');
+});
